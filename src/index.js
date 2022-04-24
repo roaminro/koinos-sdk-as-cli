e03a1e184aedddf9b771751f64529e7a75553bbb
roaminroe <roamin@tuta.io>
2022-04-24 14:35:37 -0400

    make sdk and cli version dynamic

--- src/index.js
+++ src/index.js
@@ -60,6 +60,8 @@ function createDirectoryContents (templatePath, projectName, contractName, proto
       let content = fs.readFileSync(origFilePath, 'utf8')
       content = content.replaceAll('##_CONTRACT_NAME_##', contractName)
       content = content.replaceAll('##_PROTO_PACKAGE_##', protoPackageName)
+      content = content.replaceAll('##_CLI_VERSION_##', packageJson.version)
+      content = content.replaceAll('##_SDK_VERSION_##', packageJson.devDependencies['koinos-sdk-as'])
 
       // write file to destination folder
       const writePath = path.join(CURR_DIR, projectName, destFile)
